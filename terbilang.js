30ca0039ea518bc9cebc1809c47e04e5345f81f3
Mulia Arifandy Nasution <mul14.net@gmail.com>
2014-06-29 02:29:53 +0700

    Add terbilang functionality

--- terbilang.js
+++ terbilang.js
@@ -1,23 +1,97 @@
+/* vi: set ts=2 sw=2 tw=0 et ff=unix ft=javascript: */
+
 'use strict'
 
-module.exports = function(value)
-{
-    // Fake
-    if (value == 1) return 'satu';
-    if (value == 2) return 'dua';
-    if (value == 3) return 'tiga';
-    if (value == 4) return 'empat';
-    if (value == 5) return 'lima';
-    if (value == 6) return 'enam';
-    if (value == 7) return 'tujuh';
-    if (value == 8) return 'delapan';
-    if (value == 9) return 'sembilan';
-    if (value == 10) return 'sepuluh';
-    if (value == 11) return 'sebelas';
-    if (value == 12) return 'dua belas';
-    if (value == 13) return 'tiga belas';
-    if (value == 20) return 'dua puluh';
-    if (value == 21) return 'dua puluh satu';
-    if (value == 22) return 'dua puluh dua';
-    if (value == 23) return 'dua puluh tiga';
+/**
+ * Based on http://notes.rioastamal.net/2012/03/membuat-fungsi-terbilang-pada-php.html
+ */
+
+var util = require('util');
+
+module.exports = function (value) {
+  value = parseFloat(value);
+  // array bilangan
+  // sepuluh dan sebelas merupakan special karena awalan 'se'
+  var bilangan = [
+    '',
+    'satu',
+    'dua',
+    'tiga',
+    'empat',
+    'lima',
+    'enam',
+    'tujuh',
+    'delapan',
+    'sembilan',
+    'sepuluh',
+    'sebelas'
+  ];
+
+  var division, modulus;
+
+  // pencocokan dimulai dari satuan angka terkecil
+  if (value < 12) {
+    // mapping angka ke index array bilangan
+    return bilangan[value];
+  } else if (value < 20) {
+    // bilangan 'belasan'
+    // misal 18 maka 18 - 10 = 8
+    return bilangan[value - 10] + ' belas';
+  } else if (value < 100) {
+    // bilangan 'puluhan'
+    // misal 27 maka 27 / 10 = 2.7 (integer => 2) 'dua'
+    // untuk mendapatkan sisa bagi gunakan modulus
+    // 27 mod 10 = 7 'tujuh'
+    division = parseInt(value / 10);
+    modulus = value % 10;
+    return String.prototype.trim(util.format('%s puluh %s', bilangan[division], bilangan[modulus]));
+  } else if (value < 200) {
+    // bilangan 'seratusan' (itulah indonesia knp tidak satu ratus saja? :))
+    // misal 151 maka 151 = 100 = 51 (hasil berupa 'puluhan')
+    // daripada menulis ulang rutin kode puluhan maka gunakan
+    // saja fungsi rekursif dengan memanggil fungsi terbilang(51)
+    return util.format('seratus %s', terbilang(value - 100));
+  } else if (value < 1000) {
+    // bilangan 'ratusan'
+    // misal 467 maka 467 / 100 = 4,67 (integer => 4) 'empat'
+    // sisanya 467 mod 100 = 67 (berupa puluhan jadi gunakan rekursif terbilang(67))
+    division = parseInt(value / 100);
+    modulus = value % 100;
+    return String.prototype.trim(util.format('%s ratus %s', bilangan[division], terbilang(modulus)));
+  } else if (value < 2000) {
+    // bilangan 'seribuan'
+    // misal 1250 maka 1250 - 1000 = 250 (ratusan)
+    // gunakan rekursif terbilang(250)
+    return String.prototype.trim(util.format('seribu %s', terbilang(value - 1000)));
+  } else if (value < 1000000) {
+    // bilangan 'ribuan' (sampai ratusan ribu
+    division = parseInt(value / 1000); // karena hasilnya bisa ratusan jadi langsung digunakan rekursif
+    modulus = value % 1000;
+    return util.format('%s ribu %s', terbilang(division), terbilang(modulus));
+  } else if (value < 1000000000) {
+    // bilangan 'jutaan' (sampai ratusan juta)
+    // 'satu puluh' => SALAH
+    // 'satu ratus' => SALAH
+    // 'satu juta' => BENAR
+    // @#$%^ WT*
+
+    // hasil bagi bisa satuan, belasan, ratusan jadi langsung kita gunakan rekursif
+    division = parseInt(value / 1000000);
+    modulus = value % 1000000;
+    return String.prototype.trim(util.format('%s juta %s', terbilang(division), terbilang(modulus)));
+  } else if (value < 1000000000000) {
+    // bilangan 'milyaran'
+    division = parseInt(value / 1000000000);
+    // karena batas maksimum integer untuk 32bit sistem adalah 2147483647
+    // maka kita gunakan fmod agar dapat menghandle angka yang lebih besar
+    modulus = fmod(value, 1000000000);
+    return String.prototype.trim(util.format('%s milyar %s', terbilang(division), terbilang(modulus)));
+  } else if (value < 1000000000000000) {
+    // bilangan 'triliun'
+    division = value / 1000000000000;
+    modulus = fmod(value, 1000000000000);
+    return String.prototype.trim(util.format('%s triliun %s', terbilang(division), terbilang(modulus)));
+  } else {
+    return 'Wow...';
+  }
 }
